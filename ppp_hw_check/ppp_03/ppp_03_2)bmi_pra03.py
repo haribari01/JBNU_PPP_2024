484b0b7eecbd53dcc8a172d6316f556c155d0bcd
haribari01 <153104493+haribari01@users.noreply.github.com>
2024-03-17 02:24:21 +0900

    Update ppp_03_2)bmi_pra03.py

--- ppp_hw_check/ppp_03/ppp_03_2)bmi_pra03.py
+++ ppp_hw_check/ppp_03/ppp_03_2)bmi_pra03.py
@@ -1,14 +1,3 @@
-# import ppp_02_bmi
-
-# print(ppp_02_bmi.weight)
-# print(ppp_02_bmi.height)
-''' weight = 60, height = 170 인 파일(ppp_02_bmi)을 만들고 weight,height 두 값 모두 불러오는거 성공'''
-
-# weight = int(input(ppp_02_bmi.weight))
-# height = int(input(ppp_02_bmi.height))
-# print("{}kg, {}cm.".format(weight, height))
-
-""" Q: 불러온 값을 input에 넣으면 weight값만 나오는데 height값이 안나오는 이유를 모르겠습니다."""
 import math
 
 weight = 60
